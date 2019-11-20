06e247e65885d7a4ad134487a46ea3133020a900
Katie Watts <kathryn.watts@shipk12.org>
2019-11-19 20:41:33 -0500

    Add fortunes array

--- main.js
+++ main.js
@@ -33,3 +33,16 @@ for (let i = 0; i < fortuneColor.length; i++){
   myLayerTwo[i].addEventListener('click', loopThrough);
   myLayerTwo[i].addEventListener('click', changeLayer);
 };
+
+
+
+var fortuneList=[
+  "You are on the super Naughty List",
+  "You are getting nothing but coal",
+  "You are getting nothing for Xmas",
+  "You have been super Nice",
+  "You are getting all the items on your list",
+  "Santa forgot what you want",
+  "You are on the Nice List",
+  "Rudolph told Santa your the BEST!"
+]
