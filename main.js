b043c312a701e856d7257135c498286c13071069
Katie Watts <kathryn.watts@shipk12.org>
2019-11-19 20:27:52 -0500

    Add functionlity

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 //Global Varibales
-const fortuneBox = document.getElementsByClassName('color-name');
+const firstLayer = document.getElementById('layer-one');
+const fortuneColor = document.getElementsByClassName('color-name');
+const myLayerTwo = document.getElementsByClassName('number-name');
+const secondLayer = document.getElementById('layer-two');
 
 
 //The Functionality
-function loopThrough() {
-  const blink = () => this.classList.toggle('white');
-for(let i= 0; i< this.textContent.length * 2; i++){
-  setTimeout(blink, i * 1000);
-};
+function loopThrough(){
+  const changeColor = ()=>{
+    this.classList.toggle('white');
+  };
+  for ( let i = 0; i < this.textContent.length * 2; i++){
+    setTimeout(changeColor, i * 1000);
+  }
 };
 
-//The Event Listener
-for (let i = 0; i < fortuneBox.length; i++) {
-  fortuneBox[i].addEventListener('click', loopThrough);
+
+function changeLayer(){
+  const swap = ()=>{
+    firstLayer.classList.add('wrapper-hide');
+    secondLayer.classList.remove('wrapper-hide');
+}
+setTimeout(swap, this.textContent.length * 2 * 1000);
+};
+
+
+for (let i = 0; i < fortuneColor.length; i++){
+  fortuneColor[i].addEventListener('click', loopThrough);
+  fortuneColor[i].addEventListener('click', changeLayer);
+};
+for (let i = 0; i < fortuneColor.length; i++){
+  myLayerTwo[i].addEventListener('click', loopThrough);
+  myLayerTwo[i].addEventListener('click', changeLayer);
 };
